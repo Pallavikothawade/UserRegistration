0ca7aa52954134980b34d7563fc13193abcce9fb
Pallavikothawade <Pallavijkothawade@gmail.com>
2023-01-12 16:14:10 +0530

    Adding The FirstName

---
+++ UserRagistration/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using UserRagistration;
+
+namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******Welcome The User Ragistration Program******");
+            Registration registration = new Registration();
+            registration.FirstNameUC1();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserRagistration/Registration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRagistration
+{
+    class Registration
+    {
+        public void FirstNameUC1()
+        {
+            string Pattern = "^[A-Z]{1}[a-z]{2,}$";
+            string[] FirstName = { "Pallavi" ,"Priyansh","jayesh","kushal"};
+
+            foreach (string input in FirstName)
+            {
+                if (Regex.IsMatch(input, Pattern))
+                {
+                    Console.WriteLine("{0} is valid", input);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is Invalid ", input);
+                }
+            }
+        }
+    }
+}
