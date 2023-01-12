64d01f200cd609c9c08ae51c3378109eff5a7ea1
Pallavikothawade <Pallavijkothawade@gmail.com>
2023-01-12 23:08:14 +0530

    Checking The Password

---
+++ UserRagistration/CheckingPassword.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRagistration
+{
+    class CheckingPassword
+    {
+        public void Password()
+        {
+            //UC5 Checking PassWord
+            //string Pattern = "^{8}[A-Z]{1,}[a-z]{1,}?[@,#,_,.,&,$][a-z0-9]{1,}$";
+            //string[] Password1 = { "Pallu30","Jayu123@Ko"};
+
+            string Pattern = "^{8,}[A-Z]{1,}[a-z0-9]{1,}?([.,@,-,#,&,$,*])[a-z0-9]{1,}$";
+            string[] Password1 = { "Piyu@3296", "all123@g123", "Jayu12.g123", "12pari#pari", "Jayu123@ko","Pall@12" };
+
+            foreach (string input in Password1)
+            {
+                if (Regex.IsMatch(input, Pattern))
+                {
+                    Console.WriteLine("{0} is valid", input);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is Invalid", input);
+                }
+            }
+        }
+    }
+}
--- UserRagistration/Program.cs
+++ UserRagistration/Program.cs
@@ -13,19 +13,35 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("*******Welcome The User Ragistration Program******");
-            Registration registration = new Registration();
-            registration.FirstNameUC1();
-
-            Registration2 registration2 = new Registration2();
-            registration2.LastNameUC2();
-
-            EmailChecking checkEmail = new EmailChecking();
-            checkEmail.Email();
-
-            MobileNoCheck MNC = new MobileNoCheck();
-            MNC.Mobile();
-
+            Console.WriteLine("Please Enter The choice");
+            Console.WriteLine("\n1:FirstName\n2:LastName\n3:Check Email\n4:Mobile Number\n5:Check The Password\n6:Upper Case");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Registration registration = new Registration();
+                    registration.FirstNameUC1();
+                    break;
+                case 2:
+                    Registration2 registration2 = new Registration2();
+                    registration2.LastNameUC2();
+                    break;
+                case 3:
+                    EmailChecking checkEmail = new EmailChecking();
+                    checkEmail.Email();
+                    break;
+                case 4:
+                    MobileNoCheck MNC = new MobileNoCheck();
+                    MNC.Mobile();
+                    break;
+                case 5:
+                    CheckingPassword checkingPassword = new CheckingPassword();
+                    checkingPassword.Password();
+                    break;
+                    
+            }
             Console.ReadLine();
+            
         }
     }
 }
